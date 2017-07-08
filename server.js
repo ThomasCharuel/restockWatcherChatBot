903af3b232ecdf6faba3f80d5b7afb448ad70fd1
Thomas Charuel <charuel@ece.fr>
2017-07-08 23:53:54 +0200

    setup for bot webhook

--- server.js
+++ server.js
@@ -25,6 +25,63 @@ app.get('/', function(req, res){
 });
 
 
+
+
+
+
+
+
+
+
+/*
+ * Be sure to setup your config values before running this code. You can 
+ * set them using environment variables or modifying the config file in /config.
+ *
+ */
+
+// App Secret can be retrieved from the App Dashboard
+const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ? 
+  process.env.MESSENGER_APP_SECRET :
+  config.get('appSecret');
+
+// Arbitrary value used to validate a webhook
+const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
+  (process.env.MESSENGER_VALIDATION_TOKEN) :
+  config.get('validationToken');
+
+// Generate a page access token for your page from the App Dashboard
+const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
+  (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
+  config.get('pageAccessToken');
+
+// URL where the app is running (include protocol). Used to point to scripts and 
+// assets located at this address. 
+const SERVER_URL = (process.env.SERVER_URL) ?
+  (process.env.SERVER_URL) :
+  config.get('serverURL');
+
+if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
+  console.error("Missing config values");
+  process.exit(1);
+}
+
+/*
+* Use your own validation token. Check that the token used in the Webhook 
+* setup is the same token used here.
+*
+*/
+app.get('/webhook', function(req, res) {
+if (req.query['hub.mode'] === 'subscribe' &&
+    req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+} else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+}  
+});
+
+
 // https.get('https://www.lynda.com/', res => {
 //     console.log('Response status code: ', res.statusCode);
 
