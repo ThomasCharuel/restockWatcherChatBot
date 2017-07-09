50a2668d7a4e10487280c658a10c79474160024b
Thomas Charuel <charuel@ece.fr>
2017-07-09 19:03:29 +0200

    web scrapper

---
+++ src/converse.js
@@ -0,0 +1,28 @@
+// Check and display available size for a shoe on the converse website
+// Product page example: http://www.converse.com/fr/regular/chuck-taylor-all-star-%2770/146977C_030.html?lang=fr_FR
+
+const
+    cheerio = require('cheerio');
+
+//lynx http://www.converse.com/fr/regular/chuck-taylor-all-star-%2770/146977C_030.ng=fr_FR -source
+
+var exec = require('child_process').exec;
+
+var cmd = 'lynx http://www.converse.com/fr/regular/chuck-taylor-all-star-%2770/146977C_030.ng=fr_FR -source';
+
+exec(cmd, function(error, stdout, stderr) {
+    console.log(stdout)
+    const $ = cheerio.load(stdout);
+
+    const sizesOptions = 
+        $('#sizes')
+        .children('option')
+        .filter(function(i, el){
+            return $(this).attr('value') !== '';
+        })
+        .map(function(i, el){
+            return $(this).text();
+        }).get();
+
+    sizesOptions.forEach((size) => console.log(size))
+});
